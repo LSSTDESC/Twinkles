f4f4ff2ba311c3fd4fcf782804afd7c93c905217
cmccully <cmccully@physics.rutgers.edu>
2015-02-06 15:43:49 -0800

    fixed append statement.

--- twinkles/sprinkler.py
+++ twinkles/sprinkler.py
@@ -50,7 +50,7 @@ class sprinkler():
                         lensrow['raJ2000'] += (newlens['XIMG'][i] - newlens['XSRC']) / 3600.0
                         lensrow['decJ2000'] += (newlens['YIMG'][i] - newlens['YSRC']) / 3600.0
                         lensrow['magNorm'] += newlens['MAG'][i]
-                        updated_catalog.append(lensrow)
+                        np.append(updated_catalog.append, lensrow)
 
                     # TODO: Maybe Lens original AGN or delete original source
 
