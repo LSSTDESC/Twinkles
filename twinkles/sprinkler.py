23fda3d23e8e2a2c27b3950d686cae1bbea8cc42
cmccully <cmccully@physics.rutgers.edu>
2015-02-06 15:29:41 -0800

    Fixed typo

--- twinkles/sprinkler.py
+++ twinkles/sprinkler.py
@@ -58,7 +58,7 @@ class sprinkler():
     def find_lens_candidates(self, galz):
         # search the OM10 catalog for all sources +- 0.05 in redshift from the catsim source
         w = np.where(np.abs(self.lenses['ZSRC'] - galz) <= 0.05)[0]
-        lens_candidates = self.lenses[w]
+        lens_candidates = self.lenscat[w]
 
         return lens_candidates
 
