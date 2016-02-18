36709250318ab8c3240476585863177e257abe93
R. Biswas <rbiswas4@gmail.com>
2016-02-18 15:17:55 -0800

    single line on how to run it and what it will do 	modified:   downloadInstanceCatalogs.sh

--- transfers/downloadInstanceCatalogs.sh
+++ transfers/downloadInstanceCatalogs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script will download the gzipped instance catalogs and spectra from a website. The downloaded files will be in $pwd
+# run as ./downloadInstanceCatalogs.sh
+
 curl -O http://faculty.washington.edu/rbiswas/Twinkles/InstanceCatalogs/index.txt
 file='index.txt'
 while IFS='' read -r line || [[ -n "$line" ]]; do
