683315c31ccd344a6d0241be35f146a53e6e104f
James Chiang <jchiang87@gmail.com>
2016-03-30 19:15:25 -0700

    fix conflicts with master

--- python/desc/twinkles/__init__.py
+++ python/desc/twinkles/__init__.py
@@ -1,12 +1,12 @@
 from analyseICat import *
 from calc_snr import *
 from cleanupspectra import *
-from generatePhosimInput import generatePhosimInput
-from InstcatGenerationBooKeeping import *
-from InstcatGenerator import InstcatFactory, InstcatFactory
-from sprinkler import sprinklerCompound, sprinkler
-from twinklesCatalogDefs import TwinklesCatalogZPoint
-from twinklesVariabilityMixins import TimeDelayVariability, VariabilityTwinkles
+#from generatePhosimInput import generatePhosimInput
+#from InstcatGenerationBooKeeping import *
+#from InstcatGenerator import InstcatFactory, InstcatFactory
+#from sprinkler import sprinklerCompound, sprinkler
+#from twinklesCatalogDefs import TwinklesCatalogZPoint
+#from twinklesVariabilityMixins import TimeDelayVariability, VariabilityTwinkles
 from PostageStampMaker import PostageStampMaker, create_postage_stamps
 from Display import render_fits_image
-from get_visits import get_visits
+from registry_tools import *
