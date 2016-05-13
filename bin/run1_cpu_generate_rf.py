25a4a83536cbdda883f531008ed70f914de126fe
Seth Digel <digel@stanford.edu>
2016-05-13 12:56:23 -0700

    Adopted Jim's suggested organization for the predictor class definition, cleaned up some formatting

--- bin/run1_cpu_generate_rf.py
+++ bin/run1_cpu_generate_rf.py
@@ -10,6 +10,7 @@ https://github.com/DarkEnergyScienceCollaboration/Twinkles/blob/master/examples/
 from __future__ import print_function
 import copy
 import numpy as np
+import os
 import pandas as pd
 import pickle
 from sklearn.cross_validation import train_test_split
@@ -29,29 +30,29 @@ seed = 999
 # run on a fell host, the scale factor applied was estimated from the 
 # Run 1sx times for a few visits that were re-run on hosts of different
 # classes.
-run1meta = pd.read_csv(os.path.join(os.environ['TWINKLES_DIR'], 'data', \
-    'run1_metadata_v6.csv', usecols=['filter','moonalt', 'moonphase', \
-    'cputime_fell'])
+run1meta = pd.read_csv(os.path.join(os.environ['TWINKLES_DIR'], 'data', 
+    'run1_metadata_v6.csv'),
+    usecols=['filter', 'moonalt', 'moonphase', 'cputime_fell'])
 
 # N.B. The Random Forest is used to estimate the logarithm of the CPU time,
 # which has a much smaller dynamic range
-cputime = np.log10(run1meta["cputime_fell"])
+cputime = np.log10(run1meta['cputime_fell'])
 
 # Remove the features that did not actually turn out to be useful
 # (Actually, here just remove the cputime_fell, which is the quantity to be
 # estimated from the other metadata)
 run1meta_features = copy.copy(run1meta)
-del run1meta_features["cputime_fell"]
-#del run1meta_features["runlimit"]
-#del run1meta_features["expmjd"]
-#del run1meta_features["rotskypos"]
-#del run1meta_features["altitude"]
-#del run1meta_features["rawseeing"]
-#del run1meta_features["airmass"]
-#del run1meta_features["hostname"]
-#del run1meta_features["filtskybrightness"]
-#del run1meta_features["dist2moon"]
-#del run1meta_features["sunalt"]
+del run1meta_features['cputime_fell']
+#del run1meta_features['runlimit']
+#del run1meta_features['expmjd']
+#del run1meta_features['rotskypos']
+#del run1meta_features['altitude']
+#del run1meta_features['rawseeing']
+#del run1meta_features['airmass']
+#del run1meta_features['hostname']
+#del run1meta_features['filtskybrightness']
+#del run1meta_features['dist2moon']
+#del run1meta_features['sunalt']
 
 # Define the inputs X and outputs Y and divide them both into training and
 # testing subsets
@@ -77,13 +78,13 @@ mse_RF_tuned = mean_squared_error(y_test,y_RF_tuned_pred)
 r2_RF_tuned = RF_tuned.score(X_test, y_test)
 
 # Print some metrics of the quality of the RF modeling
-print("MSE (tuned RF) =", mse_RF_tuned)
-print("R2 score (tuned RF) =",r2_RF_tuned)
+print('MSE (tuned RF) =', mse_RF_tuned)
+print('R2 score (tuned RF) =',r2_RF_tuned)
 
 RFbest = RF_tuned.best_estimator_
 
 print(run1meta_features.keys())
 print(RFbest.feature_importances_)
 
-print("Writing pickle file with the RF best estimator...")
-pickle.dump(RFbest, open("RF_pickle.p", "wb" ))
+print('Writing pickle file with the RF best estimator...')
+pickle.dump(RFbest, open('RF_pickle.p', 'wb' ))
--- python/desc/twinkles/phosim_cpu_pred.py
+++ python/desc/twinkles/phosim_cpu_pred.py
@@ -8,6 +8,7 @@ run1_cpu_generate_rf.py, using only the filter, moonalt, and moonphase
 features.  This script needs to be run only once.
 """
 
+import os
 import pickle
 import math
 import numpy as np
@@ -20,60 +21,76 @@ class CpuPred(object):
     """
     Returns predicted fell-class CPU time in seconds for user-supplied
     filter (0-5 for ugrizy), moon altitude (deg), and moon phase (0-100) 
-    -or- ObsHistID from the kraken_1042 Opsim run.  The code is hardwired to
-    look for the sqlite database for kraken_1042 in a specific location on
-    SLAC Linux.
+    -or- ObsHistID from the kraken_1042 Opsim run.  By default the code
+    looks for the sqlite database for kraken_1042 in a specific location on
+    SLAC Linux.  Also by default it looks only for obsHistID values that are
+    in the Twinkles Run 1 field (by selecting on the corresponding fieldID).
 
     RF_pickle.p is written by run1_cpu_generate_rf.py
     """
     def __init__(self, rf_pickle_file='RF_pickle.p', 
-        opsim_db_file='/nfs/farm/g/lsst/u1/DESC/Twinkles/kraken_1042_sqlite.db'):
+        opsim_db_file='/nfs/farm/g/lsst/u1/DESC/Twinkles/kraken_1042_sqlite.db',
+            fieldID=1427):
         self.RFbest = pickle.load(open(rf_pickle_file, 'rb'))
         factory = SqliteDataFrameFactory(opsim_db_file)
         self.obs_conditions = factory.create('obsHistID filter moonAlt moonPhase'.split(), 'Summary',
-            condition="where fieldID=1427")
+            condition='where fieldID=%d'%fieldID)
 
     def __call__(self, obsid):
+        """
+        Return the predicted CPU time given an obsHistID.  The obsHistID 
+        must be in the Opsim database file and fieldID specified on
+        initialization of the instance.
+        """
+        filter_index, moonalt, moonphase = self.conditions(obsid)
+        return self.cputime(filter_index, moonalt, moonphase)
+
+    def conditions(self, obsid):
+        """
+        Return the relevant observing conditions (i.e., those that are
+        inputs to the Random Forest predictor) for the given obsHistID
+        """
         rec = self.obs_conditions[self.obs_conditions['obsHistID'] == obsid]
 
         if rec.size <> 0:
             # Translate the filter string into an index 0-5
             filter_index = 'ugrizy'.find(rec['filter'].values[0])
 
-            moonalt = math.degrees(rec["moonAlt"].values[0])
-            moonphase = rec["moonPhase"].values[0]
+            moonalt = math.degrees(rec['moonAlt'].values[0])
+            moonphase = rec['moonPhase'].values[0]
         else:
-            raise RuntimeError("%d is not a Run 1 obsHistID in field 1742"%obsid)
+            raise RuntimeError('%d is not a Run 1 obsHistID in field %d'%obsid,fieldID)
 
         return filter_index, moonalt, moonphase
 
-    def conditions(self, filter_arg, moonalt_arg, moonphase_arg):
-        return 10.**self.RFbest.predict(np.array([[filter_arg, moonalt_arg, 
-            moonphase_arg]]))
+    def cputime(self, filter_index, moonalt, moonphase):
+        return 10.**self.RFbest.predict(np.array([[filter_index, moonalt, 
+            moonphase]]))
 
 if __name__ == '__main__':
     # Here are some dumb examples
     pred = CpuPred()
-    print(pred.conditions(3,10,50.))
+    print(pred(210))
 
-    print(pred(200))
+    print(pred.cputime(3.,10.,50.))
 
     # This one won't work
-    #pred.opsimdb(-999)
+    #pred(-999)
 
     # Extract the Run 1 metadata and evaluate the predicted CPU times
-    run1meta = pd.read_csv("../../../data/run1_metadata_v6.csv",
-        usecols=["filter","moonalt","moonphase","cputime_fell"])
+    run1meta = pd.read_csv(os.path.join(os.environ['TWINKLES_DIR'], 'data',
+        'run1_metadata_v6.csv'),
+        usecols=['filter', 'moonalt','moonphase','cputime_fell'])
 
-    filter = np.array(run1meta["filter"])
-    moonalt = np.array(run1meta["moonalt"])
-    moonphase = np.array(run1meta["moonphase"])
-    actual = np.array(run1meta["cputime_fell"])
+    filter = np.array(run1meta['filter'])
+    moonalt = np.array(run1meta['moonalt'])
+    moonphase = np.array(run1meta['moonphase'])
+    actual = np.array(run1meta['cputime_fell'])
 
     predicted = np.zeros(filter.size,dtype=float)
 
     for i in range(filter.size):
-        predicted[i] = pred.conditions(filter[i],moonalt[i],moonphase[i])
+        predicted[i] = pred.cputime(filter[i],moonalt[i],moonphase[i])
 
     plt.scatter(np.log10(actual), np.log10(predicted))
     plt.plot([4,6.5],[4,6.5])
