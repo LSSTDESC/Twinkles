3f5179dc0a564059c9c928da5ddaca97d3109dad
Jim Chiang <jchiang@slac.stanford.edu>
2016-09-15 16:26:03 -0700

    update to produce entries for all six LSST bands

--- bin/make_reference_catalog.py
+++ bin/make_reference_catalog.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+"""
+This script will query the CatSim database for stars in the Twinkles
+field and produce a reference catalog that can be used to generate
+astrometry.net index files.
+"""
 from __future__ import absolute_import
 import argparse
 import numpy
@@ -8,10 +13,10 @@ from lsst.sims.catUtils.utils import ObservationMetaDataGenerator
 from lsst.sims.catUtils.mixins import AstrometryStars, PhotometryStars
 
 class TwinklesReference(InstanceCatalog, AstrometryStars, PhotometryStars):
+    "Reference stars for Twinkles astrometry."
     catalog_type = 'twinkles_ref_star'
-    column_outputs = ['uniqueId', 'raJ2000', 'decJ2000', 'lsst_g',
-                      'lsst_r', 'lsst_i', 'starnotgal',
-                      'isvariable']
+    column_outputs = ['uniqueId', 'raJ2000', 'decJ2000', 'lsst_u', 'lsst_g',
+                      'lsst_r', 'lsst_i', 'lsst_z', 'lsst_y', 'isvariable']
     default_columns = [('isresolved', 0, int), ('isvariable', 0, int)]
     default_formats = {'S': '%s', 'f': '%.8f', 'i': '%i'}
     transformations = {'raJ2000': numpy.degrees, 'decJ2000': numpy.degrees}
@@ -21,6 +26,10 @@ if __name__ == '__main__':
     parser.add_argument('opsimDB', help='OpSim database sqlite file')
     parser.add_argument('-o', '--outfile', type=str, default='twinkles_ref.txt',
                         help='Filename of output reference catalog')
+    parser.add_argument('--host', type=str,
+                        default='fatboy.phys.washington.edu',
+                        help='CatSim db host name')
+    parser.add_argument('--port', type=int, default=1433, help='db host port')
     args = parser.parse_args()
 
     # you need to provide ObservationMetaDataGenerator with the connection
@@ -28,13 +37,17 @@ if __name__ == '__main__':
     # to a test database that comes when you install CatSim.
     generator = ObservationMetaDataGenerator(database=args.opsimDB,
                                              driver='sqlite')
-    obsMetaDataResults = generator.getObservationMetaData(fieldRA=(53, 54), fieldDec=(-29, -27), boundLength=0.3)
+    obsMetaDataResults = generator.getObservationMetaData(fieldRA=(53, 54),
+                                                          fieldDec=(-29, -27),
+                                                          boundLength=0.3)
 
-    # First get the reference catalog
-    stars = CatalogDBObject.from_objid('allstars')
+    stars = CatalogDBObject.from_objid('allstars', host=args.host,
+                                       port=args.port)
+
     while True:
         try:
-            ref_stars = TwinklesReference(stars, obs_metadata=obsMetaDataResults[0])
+            ref_stars = TwinklesReference(stars,
+                                          obs_metadata=obsMetaDataResults[0])
             break
         except RuntimeError:
             continue
