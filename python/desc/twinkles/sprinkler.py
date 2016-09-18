Fixed magnorm calculation.

python/desc/twinkles/sprinkler.py
@@ -29,7 +29,7 @@ class sprinklerCompound(GalaxyTileCompoundObj):
                 results[name] = np.radians(results[name])
 
         #Use Sprinkler now
-        sp = sprinkler(results, density_param = 0.5)
+        sp = sprinkler(results, density_param = 1.0)
         results = sp.sprinkle()
 
         return results
@@ -73,8 +73,15 @@ class sprinkler():
         agn_fname = str(getPackageDir('sims_sed_library') + '/agnSED/agn.spec.gz')
         agn_sed.readSED_flambda(agn_fname)
         src_iband = self.lenscat['MAGI_IN']
-        self.src_mag_norm = matchBase().calcMagNorm(src_iband, agn_sed,
-                                                    self.bandpassDict)
+        self.src_mag_norm = []
+        for src in src_iband:
+            self.src_mag_norm.append(matchBase().calcMagNorm([src],
+                                                             agn_sed,
+                                                             self.bandpassDict))
+        #self.src_mag_norm = matchBase().calcMagNorm(src_iband,
+        #                                            [agn_sed]*len(src_iband),
+        #                                            self.bandpassDict)
+
 
     def sprinkle(self):
         # Define a list that we can write out to a text file
@@ -169,7 +176,7 @@ class sprinkler():
     def find_lens_candidates(self, galz, gal_mag):
         # search the OM10 catalog for all sources +- 0.05 in redshift from the catsim source
         w = np.where((np.abs(self.lenscat['ZSRC'] - galz) <= 0.05) &
-                     (np.abs(self.src_mag_norm - gal_mag) <= 0.25))[0]
+                     (np.abs(self.src_mag_norm - gal_mag) <= 0.5))[0]
         lens_candidates = self.lenscat[w]
 
         return lens_candidates