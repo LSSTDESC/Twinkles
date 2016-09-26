b752eeb92bd2c552b503a103059d44d8f19d0898
R. Biswas <rbiswas4@gmail.com>
2016-09-26 14:14:30 -0700

    Changed TwinklesCatalogDefs to use most of the properties of PhoSimCatalogZPoint by inheritance. - running generatePhosimInput.py - Gets rid of code duplication -  Fixes bug pointed out by @danielsf in #326
    Fixes #26
    	modified:   ../twinklesCatalogDefs.py

--- python/desc/twinkles/twinklesCatalogDefs.py
+++ python/desc/twinkles/twinklesCatalogDefs.py
@@ -1,28 +1,12 @@
-"""Instance Catalog"""
+"""PhoSim Instance Catalog"""
 from __future__ import absolute_import, division, print_function
-import numpy
-from lsst.sims.utils import SpecMap, defaultSpecMap
-from lsst.sims.catalogs.definitions import InstanceCatalog
-from lsst.sims.utils import arcsecFromRadians
-from lsst.sims.catUtils.exampleCatalogDefinitions.phoSimCatalogExamples import PhosimInputBase
-from lsst.sims.catUtils.mixins import PhoSimAstrometryGalaxies, \
-                                      EBVmixin, VariabilityStars
+from lsst.sims.catUtils.exampleCatalogDefinitions import PhoSimCatalogZPoint
 from twinklesVariabilityMixins import VariabilityTwinkles
 
-class TwinklesCatalogZPoint(PhosimInputBase, PhoSimAstrometryGalaxies, EBVmixin, VariabilityTwinkles):
+class TwinklesCatalogZPoint(PhoSimCatalogZPoint, VariabilityTwinkles):
+    """
+    PhoSim Instance Catalog Class for strongly lensed (and therefore time-delayed)
+    AGN
+    """
 
     catalog_type = 'twinkles_catalog_ZPOINT'
-    column_outputs = ['prefix', 'uniqueId','raPhoSim','decPhoSim','phoSimMagNorm','sedFilepath',
-                      'redshift','shear1','shear2','kappa','raOffset','decOffset',
-                      'spatialmodel','galacticExtinctionModel','galacticAv','galacticRv',
-                      'internalExtinctionModel']
-    default_columns = [('shear1', 0., float), ('shear2', 0., float), ('kappa', 0., float),
-                       ('raOffset', 0., float), ('decOffset', 0., float), ('spatialmodel', 'ZPOINT', (str, 6)),
-                       ('galacticExtinctionModel', 'CCM', (str,3)),
-                       ('galacticAv', 0.1, float),
-                       ('galacticRv', 3.1, float),
-                       ('internalExtinctionModel', 'none', (str,4))]
-    default_formats = {'S':'%s', 'f':'%.9g', 'i':'%i'}
-    delimiter = " "
-    spatialModel = "point"
-    transformations = {'raPhoSim':numpy.degrees, 'decPhoSim':numpy.degrees}
