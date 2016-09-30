b81fa604b655acbeaf049f2b66b3ef03569be076
R. Biswas <rbiswas4@gmail.com>
2016-09-30 14:24:02 -0700

    changed generatePhosimInput according to requested changes

--- bin/generatePhosimInput.py
+++ bin/generatePhosimInput.py
@@ -3,20 +3,14 @@ Script to run the generation of phoSim instance catalogs
 """
 from __future__ import with_statement, absolute_import, division, print_function
 import os
-from lsst.sims.catUtils.utils import ObservationMetaDataGenerator
-from desc.twinkles import TwinklesSky
-import argparse
 import pandas as pd
 from sqlalchemy import create_engine
 import time
+from lsst.sims.catUtils.utils import ObservationMetaDataGenerator
+from desc.twinkles import TwinklesSky
+import argparse
 
-# Choose OpSim Output database
-OpSimDB = 'minion_1016_sqlite.db'
-# Combine the environment variable setup using `source setup/setup_locations.sh`
-# after modifying the variable
-opSimDBPath = os.path.join(os.environ['OpSimDir'], OpSimDB)
-engine = create_engine('sqlite:///' + opSimDBPath)
-obsHistIDList =  [230]
+
 
 def phoSimInputFileName(obsHistID,
                         prefix='InstanceCatalogs/PhoSim_input',
@@ -49,21 +43,9 @@ def _sql_constraint(obsHistIDList):
     sql_string += ')'
     return sql_string
 
-obs_gen = ObservationMetaDataGenerator(database=opSimDBPath)
-
-# Read in all of the OpSim records relevant for Twinkles or from a list
-# of obsHistID (remove limit 4 part of the query
-df = pd.read_sql_query('SELECT * FROM Summary WHERE FieldID == 1427 and propID in (54, 56) limit 4', engine).drop_duplicates(subset='obsHistID')
-sql_query = _sql_constraint(obsHistIDList)
-# Use if you want to use a list of obsHistIDs
-# df = pd.read_sql_query(sql_query, engine)
-recs = df.to_records()
-obsMetaDataResults = []
-obsMetaDataResults = obs_gen.ObservationMetaDataFromPointingArray(recs)
-sn_sed_file_dir = 'spectra_files'
-
-availConns = None
-for obs_metaData in obsMetaDataResults:
+def generateSinglePointing(obs_metaData, availableConns, sntable,
+                            fname,
+                            sn_sed_file_dir):
     tstart = time.time()
     obs_metaData.boundLength = 0.3
     print(obs_metaData.summary)
@@ -72,12 +54,12 @@ for obs_metaData in obsMetaDataResults:
     # all but first two are default values of optional parameters
     # Kept in script to emphasize inputs
     tSky = TwinklesSky(obs_metadata=obs_metaData,
-                       availableConnections=availConns,
+                       availableConnections=availableConns,
                        brightestStar_gmag_inCat=11.0,
                        brightestGal_gmag_inCat=11.0,
                        sntable='TwinkSN',
                        sn_sedfile_prefix=os.path.join(sn_sed_file_dir, 'specFile_'))
-    fname = phoSimInputFileName(obsHistID)  
+    #fname = phoSimInputFileName(obsHistID)  
     if not os.path.exists(os.path.dirname(fname)):
         os.makedirs(os.path.dirname(fname))
     if not os.path.exists(sn_sed_file_dir):
@@ -86,3 +68,47 @@ for obs_metaData in obsMetaDataResults:
     availConns = tSky.availableConnections
     tend = time.time()
     print (obsHistID, tend - tstart)
+
+
+if __name__ == '__main__':
+     
+    parser = argparse.ArgumentParser(description='Write phoSim Instance Catalogs'
+                                     'and SN spectra to disk')
+    parser.add_argument('--opsimDB',
+                       type=str,
+                       help='OpSim database sqlite filename',
+                       default='minion_1016_sqlite.db')
+    parser.add_argument('visit',
+                        type=int,
+                        help='Visit number (obsHistID)')
+    parser.add_argument('--OpSimDBDir',
+                        help='absolute path to dir with the opsimBD',
+                        type=str,
+                        default='./')
+    parser.add_argument('--outfile', type=str, default=None,
+                        help='output filename for instance catalog')
+    parser.add_argument('--seddir',
+                        type=str,
+                        default=None,
+                        help='directory to contain SED files')
+    args = parser.parse_args()
+
+    # Set up OpSim database
+    opSimDBPath = os.path.join(args.OpSimDBDir, args.opsimDB)
+    engine = create_engine('sqlite:///' + opSimDBPath)
+    
+    obs_gen = ObservationMetaDataGenerator(database=opSimDBPath)
+    sql_query = 'SELECT * FROM Summary WHERE ObsHistID == {}'.format(args.visit) 
+    df = pd.read_sql_query(sql_query, engine)
+    recs = df.to_records()
+    obsMetaDataResults = obs_gen.ObservationMetaDataFromPointingArray(recs)
+    obs_metaData = obsMetaDataResults[0]
+    sn_sed_file_dir = os.path.join(args.seddir, 'spectra_files')
+    
+    availConns = None
+    
+    generateSinglePointing(obs_metaData,
+                           availableConns=availConns,
+                           sntable='TwinkSN',
+                           fname=args.outfile,
+                           sn_sed_file_dir=sn_sed_file_dir)
