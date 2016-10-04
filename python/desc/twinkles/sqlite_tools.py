f7553fda8095ab76a508290c3284bf4c5313686a
Jim Chiang <jchiang@slac.stanford.edu>
2016-10-04 10:40:54 -0700

    add get_twinkles_visits function and command line executable and test code

--- python/desc/twinkles/sqlite_tools.py
+++ python/desc/twinkles/sqlite_tools.py
@@ -5,6 +5,8 @@ from __future__ import absolute_import
 import sqlite3
 import pandas as pd
 
+__all__ = ['SqliteDataFrameFactory', 'get_twinkles_visits']
+
 class SqliteDataFrameFactory(object):
     """
     Class to generate pandas data frames from columns in an sqlite3 db.
@@ -29,3 +31,30 @@ class SqliteDataFrameFactory(object):
             query += ' ' + condition
         data = [row for row in self.conn.execute(query)]
         return pd.DataFrame(data=data, columns=columns)
+
+def get_twinkles_visits(opsim_db_file, fieldID=1427):
+    """
+    Return a sorted list of visits for a given fieldID from an OpSim
+    db file.
+
+    Parameters
+    ----------
+    obsim_db_file : str
+        Filename of the OpSim db sqlite file
+    fieldID : int, optional
+        ID number of the field for which to return the visit numbers.
+        The default value is the Twinkles field.
+
+    Returns
+    -------
+    list : A list of obsHistIDs (i.e., visit numbers).
+
+    Since a given visit can belong to more than one proposal, there
+    may be multiple rows in the Summary table with the corresponding
+    obsHistID.  This function removes the duplicates.
+    """
+    connect = sqlite3.connect(opsim_db_file)
+    query = """select distinct obsHistID from Summary where fieldID=%i
+               order by obsHistID asc""" % fieldID
+    obsHistIDs = [x[0] for x in connect.execute(query)]
+    return obsHistIDs
