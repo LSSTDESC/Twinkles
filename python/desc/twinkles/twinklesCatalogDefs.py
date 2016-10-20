3bcf35bca04ac9e4e2549677d43e1f7ef7d14125
R. Biswas <rbiswas4@gmail.com>
2016-10-20 14:38:39 -0700

    Added missing __all__ to fix import 	modified:   twinklesCatalogDefs.py

--- python/desc/twinkles/twinklesCatalogDefs.py
+++ python/desc/twinkles/twinklesCatalogDefs.py
@@ -1,13 +1,11 @@
 """PhoSim Instance Catalog"""
 from __future__ import absolute_import, division, print_function
 from lsst.sims.catUtils.exampleCatalogDefinitions import PhoSimCatalogZPoint
-from twinklesVariabilityMixins import VariabilityTwinkles
-import numpy
+from .twinklesVariabilityMixins import VariabilityTwinkles
 
 class TwinklesCatalogZPoint(PhoSimCatalogZPoint, VariabilityTwinkles):
     """
     PhoSim Instance Catalog Class for strongly lensed (and therefore time-delayed)
     AGN
     """
-
     catalog_type = 'twinkles_catalog_ZPOINT'
