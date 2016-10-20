d9e97797eb0fc78c8c7e1b8a9b974031c090e0ef
R. Biswas <rbiswas4@gmail.com>
2016-10-20 15:19:29 -0700

    Fix #351 : Added `TwinklesPhoSimCatalogSN` to change output columns
    - Define `TwinklesPhoSimCatalogSN` to inherit all properties from `PhoSimCatalogSN`
    - Define new getter for `shorterFileName` : This splits sedFilepath on
      `spectra_files/specFile_` and prepends this string to the last element
       of the split list.
    - re_assign `self.column_outputs` so that its element `sedFilepath` is
      replaced by `shorterFileName`
    	modified:   twinklesCatalogDefs.py

--- python/desc/twinkles/twinklesCatalogDefs.py
+++ python/desc/twinkles/twinklesCatalogDefs.py
@@ -8,7 +8,10 @@ from lsst.sims.catUtils.exampleCatalogDefinitions.phoSimCatalogExamples import P
 from lsst.sims.catUtils.mixins import PhoSimAstrometryGalaxies, \
                                       EBVmixin, VariabilityStars
 from .twinklesVariabilityMixins import VariabilityTwinkles
-__all__ = ['TwinklesCatalogZPoint']
+
+
+__all__ = ['TwinklesCatalogZPoint', 'TwinklesPhoSimCatalogSN']
+
 class TwinklesCatalogZPoint(PhosimInputBase, PhoSimAstrometryGalaxies, EBVmixin, VariabilityTwinkles):
 
     catalog_type = 'twinkles_catalog_ZPOINT'
@@ -26,3 +29,17 @@ class TwinklesCatalogZPoint(PhosimInputBase, PhoSimAstrometryGalaxies, EBVmixin,
     delimiter = " "
     spatialModel = "point"
     transformations = {'raPhoSim':numpy.degrees, 'decPhoSim':numpy.degrees}
+
+class TwinklesPhoSimCatalogSN(PhoSimCatalogSN):
+    """
+    Modification of the PhoSimCatalogSN mixin to provide shorter sedFileNames
+    by leaving out the parts of the directory name 
+    """
+    def get_shorterFileNames(self):
+        fnames = self.column_by_name('sedFilepath')
+        sep = 'spectra_files/specFile_'
+        split_names = list(sep + fname.split(sep)[-1] for fname in fnames)
+        return split_names
+
+    self.column_outputs[self.column_outputs.index('sedFilepath')] = \
+            shorterFileNames
