e6423489d155941f2f6b59f13089b383c5f75b14
R. Biswas <rbiswas4@gmail.com>
2016-10-20 18:17:13 -0700

    Modifying column outputs 	modified:   ../python/desc/twinkles/twinklesCatalogDefs.py

--- python/desc/twinkles/twinklesCatalogDefs.py
+++ python/desc/twinkles/twinklesCatalogDefs.py
@@ -1,10 +1,9 @@
 """PhoSim Instance Catalog"""
 from __future__ import absolute_import, division, print_function
 from lsst.sims.catUtils.exampleCatalogDefinitions import PhoSimCatalogZPoint
+from lsst.sims.catUtils.exampleCatalogDefinitions.phoSimCatalogExamples import PhoSimCatalogSN
 from .twinklesVariabilityMixins import VariabilityTwinkles
 __all__ = ['TwinklesCatalogZPoint', 'TwinklesPhoSimCatalogSN']
-
-class TwinklesCatalogZPoint(PhosimInputBase, PhoSimAstrometryGalaxies, EBVmixin, VariabilityTwinkles):
 
 class TwinklesCatalogZPoint(PhoSimCatalogZPoint, VariabilityTwinkles):
     """
@@ -24,5 +23,6 @@ class TwinklesPhoSimCatalogSN(PhoSimCatalogSN):
         split_names = list(sep + fname.split(sep)[-1] for fname in fnames)
         return split_names
 
-    self.column_outputs[self.column_outputs.index('sedFilepath')] = \
-            shorterFileNames
+    column_outputs = PhoSimCatalogSN.column_outputs
+    column_outputs[PhoSimCatalogSN.column_outputs.index('sedFilepath')] = \
+        'shorterFileNames'
