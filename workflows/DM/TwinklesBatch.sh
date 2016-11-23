0a02b7043584a9f9c36272033b4a92777b9fd91a
desc Pseudo User <desc@cori17.nersc.gov>
2016-11-23 13:53:09 -0800

    Do not allow multiple threads for OpenBLAS, do not setup DM if already setup

--- workflows/DM/TwinklesBatch.sh
+++ workflows/DM/TwinklesBatch.sh
@@ -13,13 +13,20 @@ export IN_DIR=${IN_DIR:-${WORK_DIR}/input}
 # Workaround for EUPS trying to write to home directory
 export HOME=`pwd`
 
+# Workaround for low level libraries such as OpenBLAS allocating many threads
+export OMP_NUM_THREADS=1 
+
 # Test which version of RHEL we are using
 majversion=$(lsb_release -rs | cut -f1 -d.)
 
 export SCRIPT=${SCRIPT_LOCATION}/${PIPELINE_PROCESS:-$1}
 
 # Set up Twinkles environment and invoke process specific script
-if [ $majversion -eq 6 ] 
+# If DM already set up then no need to do it again
+if [ -v LSST_HOME ]
+then
+set -xe; export SHELLOPTS; source ${SCRIPT} 
+elif [ $majversion -eq 6 ] 
 then
 scl enable devtoolset-3 'source ${DM_DIR}/${DM_SETUP}; set -xe; export SHELLOPTS; source ${SCRIPT}'
 else
