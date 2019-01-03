ef576fbd8afe8bbfcb9180a330c850bb49827215
danielsf <scott.f.daniel@gmail.com>
2019-01-03 14:34:00 -0800

    adjust validation script to use twinkles_lenses*fits

--- workspace/magNorm/validate_twinkles_magnorms.py
+++ workspace/magNorm/validate_twinkles_magnorms.py
@@ -1,4 +1,3 @@
-import GCRCatalogs
 import astropy.io.fits as fits
 import lsst.sims.photUtils as sims_photUtils
 import numpy as np
@@ -7,27 +6,7 @@ import os
 data_dir = os.path.join(os.environ['TWINKLES_DIR'], 'data')
 assert os.path.isdir(data_dir)
 
-# load the mapping between galaxy_id and twinklesId
-# (really, I just want a list of all of the galaxies that
-# are getting replaced, so that I can get their redshifts from
-# the GCR)
-gal_to_twink_file = os.path.join(data_dir, 'cosmoDC2_v1.1.4_agn_cache.csv')
-assert os.path.isfile(gal_to_twink_file)
-gal_to_twink_dtype = np.dtype([('galaxy_id', int), ('twinkles_id', int)])
-gal_to_twink = np.genfromtxt(gal_to_twink_file,
-                             dtype=gal_to_twink_dtype,
-                             delimiter=',', skip_header=1)
-
-
-gcr_cat = GCRCatalogs.load_catalog('cosmoDC2_v1.1.4_image')
-gcr_cat_q = gcr_cat.get_quantities(['galaxy_id', 'redshift_true'])
-
-overlap = np.where(np.in1d(gcr_cat_q['galaxy_id'], gal_to_twink['galaxy_id']))
-
-galaxy_id = gcr_cat_q['galaxy_id'][overlap]
-redshift = gcr_cat_q['redshift_true'][overlap]
-
-fits_file_name = os.path.join(data_dir, 'cosmoDC2_v1.1.4_matched_AGN.fits')
+fits_file_name = os.path.join(data_dir, 'twinkles_lenses_cosmoDC2_v1.1.4.fits')
 assert os.path.isfile(fits_file_name)
 
 twinkles_data = fits.open(fits_file_name)[1].data
@@ -39,18 +18,14 @@ dust_wav = None
 max_dmag = -1.0
 with open('i_mag_validation.txt', 'w') as out_file:
     out_file.write('# g_id is shld d z\n')
-    for idx in range(len(twinkles_data['twinklesId'])):
-        t_id = twinkles_data['twinklesId'][idx]
+    for idx in range(len(twinkles_data['lens_sed'])):
         sed_name = twinkles_data['lens_sed'][idx]
         i_magnorm = twinkles_data['sed_magNorm'][idx][3]
         apmag_i = twinkles_data['APMAG_I'][idx]
         av = twinkles_data['lens_av'][idx]
         rv = twinkles_data['lens_rv'][idx]
 
-        valid = np.where(gal_to_twink['twinkles_id']==t_id)[0]
-        g_id = gal_to_twink['galaxy_id'][valid]
-        valid = np.where(galaxy_id==g_id)[0]
-        zz = redshift[valid]
+        zz = twinkles_data['ZLENS'][idx]
 
         spec = sims_photUtils.Sed()
         full_file_name = os.path.join(sed_dir, sed_name)
@@ -68,4 +43,4 @@ with open('i_mag_validation.txt', 'w') as out_file:
         if abs_d_mag > max_dmag:
             print('d_mag %e: is %e shld %e' % (d_mag, i_mag, apmag_i))
             max_dmag = abs_d_mag
-        out_file.write('%d %e %e %e %e\n' % (g_id, i_mag, apmag_i, d_mag, zz))
+        out_file.write('%e %e %e %e\n' % (i_mag, apmag_i, d_mag, zz))
