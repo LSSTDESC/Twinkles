c2338c2995fc325577544f7473b8da5a4dbd103f
danielsf <scott.f.daniel@gmail.com>
2019-05-16 11:56:10 -0700

    group tests by AGN vs SNe; only run them if the relevant objects are present

--- python/desc/twinkles/validation/validation_pipeline_module.py
+++ python/desc/twinkles/validation/validation_pipeline_module.py
@@ -75,36 +75,40 @@ def validation_pipeline(cat_folder, visit_num, sne_SED_path):
     if invalid_magnorm.any():
         raise RuntimeError("Some point sources have invalid magNorms")
 
+    # run AGN tests
     spr_agn = val_cat.process_sprinkled_agn(df_pt_src)
+    if len(spr_agn)>0:
+        agn_lens_gals = val_cat.process_agn_lenses(spr_agn, df_gal)
+        agn_location_test = val_cat.compare_agn_location(spr_agn, agn_lens_gals)
+        test_agn_inputs = val_cat.compare_agn_inputs(spr_agn, agn_lens_gals)
+        test_agn_lens_mags = val_cat.compare_agn_lens_mags(spr_agn,
+                                                           agn_lens_gals,
+                                                           visit_band)
+        test_agn_image_mags = val_cat.compare_agn_image_mags(spr_agn,
+                                                             agn_lens_gals,
+                                                             visit_mjd,
+                                                             visit_band)
 
-    agn_lens_gals = val_cat.process_agn_lenses(spr_agn, df_gal)
-    
-    agn_location_test = val_cat.compare_agn_location(spr_agn, agn_lens_gals)
-
-    spr_sne = val_cat.process_sprinkled_sne(df_pt_src, sne_SED_file_dir)
-
+    # run SNe tests
     sne_lens_gals = val_cat.process_sne_lenses(df_gal)
+    if len(sne_lens_gals)>0:
+        spr_sne = val_cat.process_sprinkled_sne(df_pt_src, sne_SED_file_dir)
+        test_sne_image_mags = val_cat.compare_sne_image_mags(spr_sne,
+                                                             sne_lens_gals,
+                                                             visit_mjd,
+                                                             visit_band)
 
-    sne_location_test = val_cat.compare_sne_location(spr_sne, sne_lens_gals)
+        sne_location_test = val_cat.compare_sne_location(spr_sne, sne_lens_gals)
 
-    test_agn_inputs = val_cat.compare_agn_inputs(spr_agn, agn_lens_gals)
+        test_sne_lens_inputs = val_cat.compare_sne_lens_inputs(sne_lens_gals)
 
-    test_sne_lens_inputs = val_cat.compare_sne_lens_inputs(sne_lens_gals)
-
-    test_sne_image_inputs = val_cat.compare_sne_image_inputs(spr_sne,
+        test_sne_image_inputs = val_cat.compare_sne_image_inputs(spr_sne,
                                                              sne_lens_gals,
                                                              visit_mjd,
                                                              sne_SED_file_dir,
                                                              sne_SED_path)
 
-    test_agn_lens_mags = val_cat.compare_agn_lens_mags(spr_agn, agn_lens_gals,
-                                                       visit_band)
+        test_sne_lens_mags = val_cat.compare_sne_lens_mags(sne_lens_gals,
+                                                           visit_band)
 
-    test_sne_lens_mags = val_cat.compare_sne_lens_mags(sne_lens_gals,
-                                                       visit_band)
 
-    test_agn_image_mags = val_cat.compare_agn_image_mags(spr_agn, agn_lens_gals,
-                                                         visit_mjd, visit_band)
-
-    test_sne_image_mags = val_cat.compare_sne_image_mags(spr_sne, sne_lens_gals,
-                                                         visit_mjd, visit_band)
